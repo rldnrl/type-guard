c52a5aaf841c9630e74ded118ea8a478c3e7ab8d
enginecode-cpu <bassmatics1002@gmail.com>
2021-10-11 16:13:52 +0900

    feat: export file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as isString } from "./is-string";
+export { default as isNumber } from "./is-number";
+export { default as isBoolean } from "./is-boolean";
+export { default as isUndefined } from "./is-undefined";
